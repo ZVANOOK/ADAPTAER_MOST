6467cade1ebe9b73fa5e2f115fb091e341fd206c
ZakharovVA4 <ZakharovVA4@it.mos.ru>
2024-12-05 10:48:31 +0300

    Поправил имена пакетов

--- src/main/java/spaceship/Main.java
+++ src/main/java/spaceship/Main.java
@@ -6,7 +6,7 @@ import spaceship.operations.MovableAdapterGenerator;
 public class Main {
     public static void main(String[] args) {
         String adapterClassName = "MovableAdapter";
-        String interfaceName = "Spaceship.Operations.IMovable";
+        String interfaceName = "spaceship.operations.IMovable";
 
         String generatedAdapter = MovableAdapterGenerator.generateAdapter(adapterClassName, interfaceName);
         System.out.println("Генерируемый адаптер: " + generatedAdapter);
