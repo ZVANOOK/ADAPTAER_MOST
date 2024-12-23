3467a87a29f97f3505b8067c8406715bb636ba81
ZakharovVA4 <ZakharovVA4@it.mos.ru>
2024-12-23 15:10:18 +0300

    Изменил MovableAdapterGenerator

--- src/main/java/Spaceship/Operations/IoC.java
+++ src/main/java/Spaceship/Operations/IoC.java
@@ -2,8 +2,19 @@ package Spaceship.Operations;
 
 public class IoC {
     public static Object Resolve(String key, Object... args) {
-        // Ваша реализация для возврата экземпляра объекта по ключу
-        // Для тестирования можно просто возвращать новый экземпляр Vector
+        if (key.equals("Adapter")) {
+            if (args[0] instanceof Class && args.length > 1 && args[1] instanceof UObject) {
+                Class<?> adapterClass = (Class<?>) args[0];
+                UObject obj = (UObject) args[1];
+                try {
+                    // Создаем экземпляр адаптера с использованием рефлексии
+                    return adapterClass.getDeclaredConstructor(UObject.class).newInstance(obj);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }
         if (key.contains("position")) {
             return new Vector(0, 0); // Возвращаем начальную позицию
         } else {
